bfe2f81b1291ccddd9c35aeecd93821a1a8a624a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 08:45:41 +0800

    Create dependency.js

---
+++ src/internal/dependency.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-cycle */
+export { default as concat } from './concat';
+export { default as filter } from './filter';
+export { default as first } from './first';
+export { default as flat } from './flat';
+export { default as last } from './last';
+export { default as just } from './just';
+export { default as map } from './map';
+export { default as repeat } from './repeat';
+export { default as startWith } from './startWith';
+export { default as zip } from './zip';
