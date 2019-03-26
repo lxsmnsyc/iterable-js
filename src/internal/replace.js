f26a7968c35929b81c0a3659ad34ae0cffc07044
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 14:44:19 +0800

    Create replace.js

---
+++ src/internal/replace.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable func-names */
+import { isIterable, BadArgumentError, isNumber } from './utils';
+import Iterable from '../iterable';
+
+const replace = (iterable, index, value) => {
+  if (!isIterable(iterable)) {
+    throw new BadArgumentError(1, 'Iterable.replace', 'Iterable');
+  }
+  if (!isNumber(index)) {
+    throw new BadArgumentError(2, 'Iterable.replace', 'number');
+  }
+  if (index < 0) {
+    throw new BadArgumentError(2, 'Iterable.replace', 'positive number');
+  }
+
+  return new Iterable(function* () {
+    let c = 0;
+
+    for (const i of iterable) {
+      if (c === index) {
+        yield value;
+      } else {
+        yield i;
+      }
+      c += 1;
+    }
+  });
+};
+
+export default replace;
