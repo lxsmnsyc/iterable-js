41a5fb34c1be23c680892500df8b29e32147379e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 14:58:43 +0800

    Create range.js

---
+++ src/internal/range.js
@@ -0,0 +1,32 @@
+/* eslint-disable func-names */
+import { isNumber, isUndefined } from 'util';
+import { BadArgumentError } from './utils';
+import Iterable from '../iterable';
+
+const range = (start, end, steps) => {
+  if (!isNumber(start)) {
+    throw new BadArgumentError(1, 'Iterable.range', 'number');
+  }
+  if (!isNumber(end)) {
+    throw new BadArgumentError(2, 'Iterable.range', 'number');
+  }
+
+  let step = steps;
+
+  if (!isUndefined(steps)) {
+    if (!isNumber(steps)) {
+      throw new BadArgumentError(3, 'Iterable.range', 'number or undefined');
+    }
+  } else {
+    step = 1;
+  }
+
+  return new Iterable(function* () {
+    const direction = step * Math.sign(end - start);
+    for (let c = start; (direction < 0 ? c >= end : c <= end); c += direction) {
+      yield c;
+    }
+  });
+};
+
+export default range;
