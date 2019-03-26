45062f051178836ae3ac5000923d2db05464632a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 14:43:46 +0800

    Update contains.js

--- src/internal/contains.js
+++ src/internal/contains.js
@@ -1,6 +1,6 @@
 /* eslint-disable func-names */
 /* eslint-disable no-restricted-syntax */
-import { isIterable } from './utils';
+import { isIterable, BadArgumentError } from './utils';
 import find from './find';
 import map from './map';
 /**
@@ -8,7 +8,7 @@ import map from './map';
  */
 const contains = (iterable, value) => {
   if (!isIterable(iterable)) {
-    throw new TypeError('bad argument #1 to Iterable.find (Iterable expected)');
+    throw new BadArgumentError(1, 'Iterable.contains', 'Iterable');
   }
   return map(find(iterable, value), x => x === value);
 };
