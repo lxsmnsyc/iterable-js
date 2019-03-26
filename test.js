341af89f5e501b16183976af8fc0dc08486fbf56
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 13:36:37 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-restricted-syntax */
 const Iterable = require('./index.js');
 
-for (const c of Iterable.onDone([1, 2, 3, 4, 5], () => console.log('Done!'))) {
-  console.log('Yield: ', c);
+const evenOdd = Iterable.partition([1, 2, 3, 4, 5], x => x < 3);
+for (const c of evenOdd[0]) {
+  console.log('1', c);
+}
+for (const c of evenOdd[1]) {
+  console.log('2', c);
 }
