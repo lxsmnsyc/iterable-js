cc6d255a0b5ba98d6265ce7a5fc5d3502fad9d0e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 14:43:42 +0800

    Update all.js

--- src/internal/all.js
+++ src/internal/all.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable func-names */
 import Iterable from '../iterable';
-import { isIterable } from './utils';
+import { isIterable, BadArgumentError, isFunction } from './utils';
 /**
  * @ignore
  */
 const all = (iterable, predicate) => {
   if (!isIterable(iterable)) {
-    throw new TypeError('bad argument #1 to Iterable.all (Iterable expected)');
+    throw new BadArgumentError(1, 'Iterable.all', 'Iterable');
   }
-  if (typeof predicate !== 'function') {
-    throw new TypeError('bad argument #2 to Iterable.all (function expected)');
+  if (!isFunction(predicate)) {
+    throw new BadArgumentError(2, 'Iterable.all', 'function');
   }
   return new Iterable(function* () {
     for (const i of iterable) {
