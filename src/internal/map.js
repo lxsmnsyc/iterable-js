e903a72bec85234974ef4e4011853e0ae1cf8c2a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 14:44:06 +0800

    Update map.js

--- src/internal/map.js
+++ src/internal/map.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable func-names */
 import Iterable from '../iterable';
-import { isIterable } from './utils';
+import { isIterable, BadArgumentError, isFunction } from './utils';
 
 /**
  * @ignore
  */
 const map = (iterable, mapper) => {
-  if (!(isIterable(iterable))) {
-    throw new TypeError('bad argument #1 to Iterable.map (Iterable expected)');
+  if (!isIterable(iterable)) {
+    throw new BadArgumentError(1, 'Iterable.map', 'Iterable');
   }
-  if (typeof mapper !== 'function') {
-    throw new TypeError('bad argument #2 to Iterable.map (function expected)');
+  if (!isFunction(mapper)) {
+    throw new BadArgumentError(2, 'Iterable.map', 'function');
   }
   return new Iterable(function* () {
     for (const i of iterable) {
