fa3e2d8e68f473e746d2c17bad76040fdd8c25c6
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 14:44:41 +0800

    Update utils.js

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -5,8 +5,24 @@
  * @ignore
  */
 export const ITERATOR = Symbol.iterator;
-
 /**
  * @ignore
  */
 export const isIterable = x => typeof x[ITERATOR] === 'function';
+/**
+ * @ignore
+ */
+export class BadArgumentError extends TypeError {
+  constructor(argumentNo, methodName, expectedType) {
+    super();
+    this.message = `bad argument #${argumentNo} to ${methodName} (${expectedType} expected)`;
+  }
+}
+/**
+ * @ignore
+ */
+export const isFunction = x => typeof x === 'function';
+/**
+ * @ignore
+ */
+export const isNumber = x => typeof x === 'number';
