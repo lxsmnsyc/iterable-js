fd57b07480a7aa9751d2399377195e290bca4da9
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 10:20:25 +0800

    Update utils.js

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -10,12 +10,3 @@ export const ITERATOR = Symbol.iterator;
  * @ignore
  */
 export const isIterable = x => typeof x[ITERATOR] === 'function';
-
-/**
- * @ignore
- */
-export const toGenerator = (iterable, each) => function* () {
-  for (const i of iterable) {
-    yield each(i);
-  }
-};
