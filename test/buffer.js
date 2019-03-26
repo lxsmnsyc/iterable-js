64845f096bac240b7899b526ea31fad3d99173ac
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 18:03:11 +0800

    Create buffer.js

---
+++ test/buffer.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#buffer', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.buffer();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is an invalid number', () => {
+    try {
+      Iterable.buffer([1, 2, 3]);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is a non-positive number', () => {
+    try {
+      Iterable.buffer([1, 2, 3], -1);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.buffer([1, 2, 3, 4], 2);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield Arrays of the given length.', () => {
+    const iterable = Iterable.buffer([1, 2, 3, 4], 2);
+    for (const i of iterable) {
+      assert(i instanceof Array);
+      assert(i.length === 2);
+    }
+  });
+  it('should yield Arrays of the given length along with an excess.', () => {
+    const iterable = Iterable.buffer([1, 2, 3, 4, 5], 2);
+    for (const i of iterable) {
+      assert(i instanceof Array);
+    }
+  });
+});
