7c1934a797a22ff8b9a6d5682ccf77e47cdf06b7
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 16:43:20 +0800

    Create reverse.js

---
+++ src/internal/reverse.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable func-names */
+import { isIterable, BadArgumentError } from './utils';
+import Iterable from '../iterable';
+
+const reverse = (iterable) => {
+  if (!isIterable(iterable)) {
+    throw new BadArgumentError(1, 'Iterable.reverse', 'Iterable');
+  }
+
+  return new Iterable(function* () {
+    const buffer = [];
+
+    for (const i of iterable) {
+      buffer.unshift(i);
+    }
+    for (const i of buffer) {
+      yield i;
+    }
+  });
+};
+
+export default reverse;
