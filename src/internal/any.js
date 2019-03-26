716d8d5aca76453424f1e2eec18f107f9b9d7147
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 09:20:35 +0800

    Create any.js

---
+++ src/internal/any.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable func-names */
+import Iterable from '../iterable';
+import { isIterable } from './utils';
+
+const any = (iterable, predicate) => {
+  if (!isIterable(iterable)) {
+    throw new TypeError('expects an object that implements the Iteration Protocol');
+  }
+  if (typeof predicate !== 'function') {
+    throw new TypeError('expects the predicate to be a function.');
+  }
+  return new Iterable(function* () {
+    for (const i of iterable) {
+      let result;
+
+      try {
+        result = predicate(i);
+      } catch (e) {
+        yield false;
+        return;
+      }
+
+      if (result) {
+        yield true;
+        return;
+      }
+    }
+    yield false;
+  });
+};
+
+export default any;
