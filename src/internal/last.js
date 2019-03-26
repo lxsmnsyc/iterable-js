cd51a2d766be6e1e78fa0b2e8b1072e32571aa61
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 09:20:45 +0800

    Update last.js

--- src/internal/last.js
+++ src/internal/last.js
@@ -1,12 +1,13 @@
 /* eslint-disable func-names */
 /* eslint-disable no-restricted-syntax */
 import Iterable from '../iterable';
+import { isIterable } from './utils';
 
 /**
  * @ignore
  */
 const last = (iterable) => {
-  if (!(iterable instanceof Iterable)) {
+  if (!isIterable(iterable)) {
     throw new TypeError('expects an object that implements the Iteration Protocol');
   }
   return new Iterable(function* () {
