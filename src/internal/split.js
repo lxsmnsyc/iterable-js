96056c7a0d3db584ff8aea812c7cf5b36ed79f45
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 12:00:18 +0800

    Update split.js

--- src/internal/split.js
+++ src/internal/split.js
@@ -4,13 +4,13 @@ import skip from './skip';
 
 const split = (iterable, count) => {
   if (!isIterable(iterable)) {
-    throw new TypeError('expects an object that implements the Iteration Protocol');
+    throw new TypeError('bad argument #1 to Iterable.split (Iterable expected)');
   }
   if (typeof count !== 'number') {
-    throw new TypeError('expects count to be a number.');
+    throw new TypeError('bad argument #2 to Iterable.split (number expected)');
   }
   if (count < 0) {
-    throw new TypeError('expects "count" to be a positive number.');
+    throw new TypeError('bad argument #2 to Iterable.split (positive number expected)');
   }
 
   return [take(iterable, count), skip(iterable, count)];
