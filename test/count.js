b748ff1f5db52671f02ae4f8387fc3de2a57175b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 18:03:21 +0800

    Create count.js

---
+++ test/count.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#count', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.count();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable', () => {
+    assert(Iterable.count([1, 2, 3, 4]) instanceof Iterable);
+  });
+  it('should yield the correct size if the Iterable yields the value', () => {
+    for (const c of Iterable.count([1, 2, 3, 4])) {
+      assert(c === 4);
+    }
+  });
+});
