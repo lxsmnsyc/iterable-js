fe463ab26ee048e29226d318b8b647dd8047285d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 10:20:06 +0800

    Create skip.js

---
+++ src/internal/skip.js
@@ -0,0 +1,33 @@
+/* eslint-disable func-names */
+/* eslint-disable no-restricted-syntax */
+import { isIterable } from './utils';
+import Iterable from '../iterable';
+
+/**
+ * @ignore
+ */
+const skip = (iterable, count) => {
+  if (!isIterable(iterable)) {
+    throw new TypeError('expects an object that implements the Iteration Protocol');
+  }
+  if (typeof count !== 'number') {
+    throw new TypeError('expects count to be a number.');
+  }
+  if (count < 0) {
+    throw new TypeError('expects "count" to be a positive number.');
+  }
+
+  return new Iterable(function* () {
+    let c = count;
+
+    for (const i of iterable) {
+      if (c > 0) {
+        c -= 1;
+      } else {
+        yield i;
+      }
+    }
+  });
+};
+
+export default skip;
