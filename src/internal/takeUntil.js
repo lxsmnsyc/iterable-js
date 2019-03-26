aa2ede942b20e693492287ca517d08c8ec8e6534
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 13:36:33 +0800

    Create takeUntil.js

---
+++ src/internal/takeUntil.js
@@ -0,0 +1,28 @@
+/* eslint-disable func-names */
+/* eslint-disable no-restricted-syntax */
+
+import Iterable from '../iterable';
+import { isIterable } from './utils';
+
+/**
+ * @ignore
+ */
+const takeUntil = (iterable, predicate) => {
+  if (!isIterable(iterable)) {
+    throw new TypeError('bad argument #1 to Iterable.takeWhile (Iterable expected)');
+  }
+  if (typeof predicate !== 'function') {
+    throw new TypeError('bad argument #2 to Iterable.takeWhile (function expected)');
+  }
+  return new Iterable(function* () {
+    for (const i of iterable) {
+      if (!predicate(i)) {
+        yield i;
+      } else {
+        return;
+      }
+    }
+  });
+};
+
+export default takeUntil;
