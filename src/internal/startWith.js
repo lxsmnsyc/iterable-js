29bb077edc61ea71b9fd9b665e791bd84144ed6c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 14:44:32 +0800

    Update startWith.js

--- src/internal/startWith.js
+++ src/internal/startWith.js
@@ -1,11 +1,11 @@
 import concat from './concat';
-import { isIterable } from './utils';
+import { isIterable, BadArgumentError } from './utils';
 /**
  * @ignore
  */
 const startWith = (iterable, ...iterables) => {
   if (!isIterable(iterable)) {
-    throw new TypeError('bad argument #1 to Iterable.startWith (Iterable expected)');
+    throw new BadArgumentError(1, 'Iterable.startWith', 'Iterable');
   }
 
   return concat(...iterables, iterable);
