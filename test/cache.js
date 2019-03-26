5bc834f8ec66389584a27ff71707abba51aba943
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 18:03:13 +0800

    Create cache.js

---
+++ test/cache.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#cache', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.cache();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should cache the source Iterable', () => {
+    const cache1 = Iterable.cache([1, 2, 3, 4]);
+
+    let flag = 2;
+    for (const c of cache1) {
+      flag -= 1;
+      if (flag === 0) {
+        break;
+      }
+    }
+    // eslint-disable-next-line no-empty
+    for (const c of cache1) {
+    }
+  });
+});
