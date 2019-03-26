af5a9eea86018be5168fedb3336ca898dc67117f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 09:20:37 +0800

    Update dependency.js

--- src/internal/dependency.js
+++ src/internal/dependency.js
@@ -1,10 +1,13 @@
 /* eslint-disable import/no-cycle */
+export { default as all } from './all';
+export { default as any } from './any';
 export { default as concat } from './concat';
 export { default as filter } from './filter';
 export { default as first } from './first';
 export { default as flat } from './flat';
-export { default as last } from './last';
+export { default as isEmpty } from './isEmpty';
 export { default as just } from './just';
+export { default as last } from './last';
 export { default as map } from './map';
 export { default as repeat } from './repeat';
 export { default as startWith } from './startWith';
