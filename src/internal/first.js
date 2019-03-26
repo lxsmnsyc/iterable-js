c3e526dd7597fe252926dc00d344e6f3e3163dba
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-26 14:43:55 +0800

    Update first.js

--- src/internal/first.js
+++ src/internal/first.js
@@ -1,13 +1,13 @@
 /* eslint-disable func-names */
 /* eslint-disable no-restricted-syntax */
 import Iterable from '../iterable';
-import { isIterable } from './utils';
+import { isIterable, BadArgumentError } from './utils';
 /**
  * @ignore
  */
 const first = (iterable) => {
   if (!isIterable(iterable)) {
-    throw new TypeError('bad argument #1 to Iterable.first (Iterable expected)');
+    throw new BadArgumentError(1, 'Iterable.first', 'Iterable');
   }
   return new Iterable(function* () {
     for (const i of iterable) {
