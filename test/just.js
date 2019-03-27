934a045e378625fe74b6678e8510d0bb9f54a5b3
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 09:41:07 +0800

    Create just.js

---
+++ test/just.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#just', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.just();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.just(100);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct result.', () => {
+    const x = 100;
+    const iterable = Iterable.just(x);
+
+    for (const i of iterable) {
+      assert(x === i);
+    }
+  });
+});
