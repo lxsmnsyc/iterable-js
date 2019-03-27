66982537ec321839d1a7d2512d59c076a965cc5a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 09:40:56 +0800

    Create find.js

---
+++ test/find.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#find', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.find();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is an invalid predicate', () => {
+    try {
+      Iterable.find([1, 2, 3]);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.find([1, 2, 3], x => typeof x === 'number');
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct result.', () => {
+    const base = [1, 2, 3];
+    const iterable = new Iterable(base.slice(0)).find(x => x === 3);
+
+    for (const i of iterable) {
+      assert(base.indexOf(3) === i);
+    }
+  });
+});
