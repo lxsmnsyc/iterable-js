805f94119deeda6f5dc703a6418a366329dc583d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 13:51:16 +0800

    Update repeat.js

--- src/internal/repeat.js
+++ src/internal/repeat.js
@@ -1,22 +1,17 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable func-names */
-import { isIterable, BadArgumentError, isNumber } from './utils';
+import { defineField, IterablePositiveNumberCheck } from './utils';
 import Iterable from '../iterable';
 
 /**
  * @ignore
  */
-const repeat = (iterable, count) => {
-  if (!isIterable(iterable)) {
-    throw new BadArgumentError(1, 'Iterable.repeat', 'Iterable');
-  }
-  if (!isNumber(count)) {
-    throw new BadArgumentError(2, 'Iterable.repeat', 'number');
-  }
-  if (count < 0) {
-    throw new BadArgumentError(2, 'Iterable.repeat', 'positive number');
-  }
-
+const FIELD = defineField('repeat');
+/**
+ * @ignore
+ */
+export default (iterable, count) => {
+  IterablePositiveNumberCheck(iterable, count, FIELD);
   return new Iterable(function* () {
     for (let c = count; c > 0; c -= 1) {
       for (const i of iterable) {
@@ -25,5 +20,3 @@ const repeat = (iterable, count) => {
     }
   });
 };
-
-export default repeat;
