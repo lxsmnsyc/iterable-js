09c85ec9a0ecf509b4319563fb59d0366ca118a9
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 13:51:33 +0800

    Update skipWhile.js

--- src/internal/skipWhile.js
+++ src/internal/skipWhile.js
@@ -2,18 +2,16 @@
 /* eslint-disable no-restricted-syntax */
 
 import Iterable from '../iterable';
-import { isIterable, BadArgumentError, isFunction } from './utils';
-
+import { defineField, IterablePredicateCheck } from './utils';
 /**
  * @ignore
  */
-const skipWhile = (iterable, predicate) => {
-  if (!isIterable(iterable)) {
-    throw new BadArgumentError(1, 'Iterable.skipUntil', 'Iterable');
-  }
-  if (!isFunction(predicate)) {
-    throw new BadArgumentError(2, 'Iterable.skipUntil', 'function');
-  }
+const FIELD = defineField('skipWhile');
+/**
+ * @ignore
+ */
+export default (iterable, predicate) => {
+  IterablePredicateCheck(iterable, predicate, FIELD);
   return new Iterable(function* () {
     let flag = true;
     for (const i of iterable) {
@@ -26,5 +24,3 @@ const skipWhile = (iterable, predicate) => {
     }
   });
 };
-
-export default skipWhile;
