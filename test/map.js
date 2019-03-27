ebfc1061f98c455d286c2dc66a504dc01d445e36
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 09:41:12 +0800

    Create map.js

---
+++ test/map.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#map', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.map();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is an invalid predicate', () => {
+    try {
+      Iterable.map([1, 2, 3]);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.map([1, 2, 3], x => x);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct result.', () => {
+    const base = [1, 2, 3];
+    const expected = [2, 4, 6];
+    const iterable = new Iterable(base).map(x => x * 2);
+
+    for (const i of iterable) {
+      assert(expected.shift() === i);
+    }
+  });
+});
