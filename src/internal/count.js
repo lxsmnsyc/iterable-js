4f27b44f2b00946133c604bac651e44adb1c4ffe
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 13:50:05 +0800

    Update count.js

--- src/internal/count.js
+++ src/internal/count.js
@@ -1,15 +1,17 @@
 /* eslint-disable func-names */
 /* eslint-disable no-restricted-syntax */
-import { isIterable, BadArgumentError } from './utils';
+import { IterableCheck, defineField } from './utils';
 import Iterable from '../iterable';
 
 /**
  * @ignore
  */
-const count = (iterable) => {
-  if (!isIterable(iterable)) {
-    throw new BadArgumentError(1, 'Iterable.count', 'Iterable');
-  }
+const FIELD = defineField('count');
+/**
+ * @ignore
+ */
+export default (iterable) => {
+  IterableCheck(iterable, 1, FIELD);
 
   return new Iterable(function* () {
     let c = 0;
@@ -20,5 +22,3 @@ const count = (iterable) => {
     yield c;
   });
 };
-
-export default count;
