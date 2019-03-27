3cdd17922e7d72f6e4b25d51be7eb93ff5285692
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 09:40:54 +0800

    Create filter.js

---
+++ test/filter.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#filter', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.filter();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is an invalid predicate', () => {
+    try {
+      Iterable.filter([1, 2, 3]);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.filter([1, 2, 3], x => typeof x === 'number');
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct result.', () => {
+    const base = [1, 2, 3];
+    const iterable = new Iterable(base.slice(0)).filter(x => typeof x === 'number');
+
+    for (const i of iterable) {
+      assert(base.shift() === i);
+    }
+  });
+});
