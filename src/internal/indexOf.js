0da559ea4426e616bdbf28a8d149aceb10d0f68d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 13:50:46 +0800

    Update indexOf.js

--- src/internal/indexOf.js
+++ src/internal/indexOf.js
@@ -1,15 +1,15 @@
 /* eslint-disable func-names */
 /* eslint-disable no-restricted-syntax */
-import { isIterable, BadArgumentError } from './utils';
+import { defineField, IterableCheck } from './utils';
 import find from './find';
 /**
  * @ignore
  */
-const indexOf = (iterable, value) => {
-  if (!isIterable(iterable)) {
-    throw new BadArgumentError(1, 'Iterable.indexOf', 'Iterable');
-  }
+const FIELD = defineField('indexOf');
+/**
+ * @ignore
+ */
+export default (iterable, value) => {
+  IterableCheck(iterable, 1, FIELD);
   return find(iterable, x => x === value);
 };
-
-export default indexOf;
