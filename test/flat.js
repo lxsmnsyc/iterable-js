400809af086e40c5c8e9b09d1f9d8772ab3229e7
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 09:41:00 +0800

    Create flat.js

---
+++ test/flat.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#flat', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.flat();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.flat([1, 2, 3]);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct result.', () => {
+    const base = [1, 2, [3, 4, 5]];
+    const expected = [1, 2, 3, 4, 5];
+    const iterable = new Iterable(base).flat();
+
+    for (const i of iterable) {
+      assert(expected.shift() === i);
+    }
+  });
+});
