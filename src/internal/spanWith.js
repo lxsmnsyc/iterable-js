0469128f847520c3ab4e26baac457ffda2dabe6b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 13:51:41 +0800

    Update spanWith.js

--- src/internal/spanWith.js
+++ src/internal/spanWith.js
@@ -1,22 +1,18 @@
-import { isIterable, isFunction, BadArgumentError } from './utils';
+
 import skipWhile from './skipWhile';
 import takeWhile from './takeWhile';
-
+import { defineField, IterablePredicateCheck } from './utils';
 /**
  * @ignore
  */
-const spanWith = (iterable, predicate) => {
-  if (!isIterable(iterable)) {
-    throw new BadArgumentError(1, 'Iterable.spanWith', 'Iterable');
-  }
-  if (!isFunction(predicate)) {
-    throw new BadArgumentError(2, 'Iterable.spanWith', 'function');
-  }
-
+const FIELD = defineField('spanWith');
+/**
+ * @ignore
+ */
+export default (iterable, predicate) => {
+  IterablePredicateCheck(iterable, predicate, FIELD);
   return [
     takeWhile(iterable, predicate),
     skipWhile(iterable, predicate),
   ];
 };
-
-export default spanWith;
