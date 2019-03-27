1d5b7504dda68522918367f6502457d6622324fb
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 13:49:52 +0800

    Update breakWith.js

--- src/internal/breakWith.js
+++ src/internal/breakWith.js
@@ -1,21 +1,18 @@
-import { isIterable, isFunction, BadArgumentError } from './utils';
+import { IterablePredicateCheck, defineField } from './utils';
 import takeUntil from './takeUntil';
 import skipUntil from './skipUntil';
 
 /**
  * @ignore
  */
-const breakWith = (iterable, predicate) => {
-  if (!isIterable(iterable)) {
-    throw new BadArgumentError(1, 'Iterable.breakWith', 'Iterable');
-  }
-  if (!isFunction(predicate)) {
-    throw new BadArgumentError(2, 'Iterable.breakWith', 'function');
-  }
+const FIELD = defineField('breakWith');
+/**
+ * @ignore
+ */
+export default (iterable, predicate) => {
+  IterablePredicateCheck(iterable, predicate, FIELD);
   return [
     takeUntil(iterable, predicate),
     skipUntil(iterable, predicate),
   ];
 };
-
-export default breakWith;
