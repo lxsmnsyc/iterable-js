2029e583c67c667eb20480c9b2adf53742a0ec20
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 17:04:51 +0800

    Update last.js

--- src/internal/last.js
+++ src/internal/last.js
@@ -1,7 +1,9 @@
 /* eslint-disable func-names */
 /* eslint-disable no-restricted-syntax */
 import Iterable from '../iterable';
-import { defineField, IterableCheck } from './utils';
+import {
+  defineField, IterableCheck, FunctionCheck, isUndefined,
+} from './utils';
 /**
  * @ignore
  */
@@ -9,12 +11,26 @@ const FIELD = defineField('last');
 /**
  * @ignore
  */
-export default (iterable) => {
+const defaultTest = () => true;
+/**
+ * @ignore
+ */
+export default (iterable, predicate) => {
   IterableCheck(iterable, 1, FIELD);
+
+  let fn = predicate;
+
+  if (!isUndefined(fn)) {
+    FunctionCheck(predicate, 2, FIELD);
+  } else {
+    fn = defaultTest;
+  }
   return new Iterable(function* () {
     let v;
     for (const i of iterable) {
-      v = i;
+      if (fn(i)) {
+        v = i;
+      }
     }
     yield v;
   });
