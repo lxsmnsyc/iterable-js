23b34c780a72476c37519e4d95b79fd85477ede6
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 10:13:04 +0800

    Create reduce.js

---
+++ test/reduce.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#reduce', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.reduce();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is an invalid predicate', () => {
+    try {
+      Iterable.reduce([1, 2, 3]);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.reduce([1, 2, 3], (acc, i) => acc + i);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct result.', () => {
+    const base = [1, 2, 3];
+    const iterable = new Iterable(base).reduce((acc, i) => {
+      if (typeof acc === 'undefined') {
+        return i;
+      }
+      return acc + i;
+    });
+    for (const i of iterable) {
+      assert(i === base.reduce((acc, x) => acc + x));
+    }
+  });
+});
