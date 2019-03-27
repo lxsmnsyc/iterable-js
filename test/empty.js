17c6af86910d11fffe6a8597cc4cbfeeea201ca6
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 09:40:51 +0800

    Create empty.js

---
+++ test/empty.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+
+/* eslint-disable no-undef */
+describe('#empty', () => {
+  it('should be empty.', () => {
+    for (const i of Iterable.empty()) {
+      assert(!i);
+    }
+  });
+});
