273c9e047504dd07609422173d462610f2f45ec3
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 13:50:35 +0800

    Create equal.js

---
+++ src/internal/equal.js
@@ -0,0 +1,28 @@
+/* eslint-disable func-names */
+/* eslint-disable no-restricted-syntax */
+import { DoubleIterableCheck, defineField } from './utils';
+import Iterable from '../iterable';
+import toArray from './toArray';
+/**
+ * @ignore
+ */
+const FIELD = defineField('equal');
+/**
+ * @ignore
+ */
+export default (iterable, other) => {
+  DoubleIterableCheck(iterable, other, FIELD);
+
+  return new Iterable(function* () {
+    const arr = toArray(iterable);
+
+    for (const i of other) {
+      if (i !== arr.shift()) {
+        yield i;
+        return;
+      }
+    }
+
+    yield arr.length === 0;
+  });
+};
