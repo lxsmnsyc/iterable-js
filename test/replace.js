ddd472259ebf7618c35e93b40cade802849d4753
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-27 10:13:08 +0800

    Create replace.js

---
+++ test/replace.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#replace', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.replace();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is an invalid number', () => {
+    try {
+      Iterable.replace([1, 2, 3]);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is a non-positive number', () => {
+    try {
+      Iterable.replace([1, 2, 3], -1);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.replace([1, 2, 3, 4], 2);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct value', () => {
+    const base = [1, 2, 3, 4];
+    const expected = [1, 2, 5, 4];
+    const iterable = new Iterable(base).replace(2, 5);
+    for (const c of iterable) {
+      assert(c === expected.shift());
+    }
+  });
+});
