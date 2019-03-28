97ab33200328b02a43778ff23e256352c192dbe5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 13:23:15 +0800

    Create sum.js

---
+++ test/sum.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#sum', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.sum();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable', () => {
+    assert(Iterable.sum([1, 2, 3]) instanceof Iterable);
+  });
+  it('should yield the correct sum if the Iterable yields the value', () => {
+    const base = [1, 2, 3];
+    for (const c of new Iterable(base).sum()) {
+      assert(c === base.reduce((x, y) => x + y));
+    }
+  });
+});
