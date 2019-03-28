77278a8545bf32db5ab62a0abe4436187c7cc583
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:32:19 +0800

    Create breadthFirst.js

---
+++ test/breadthFirst.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#breadthFirst', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.breadthFirst();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.breadthFirst([1, 2, 3, 4]);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct value', () => {
+    const base = [1, [2, 3], 4];
+    const expected = [1, 4, 2, 3];
+    const iterable = new Iterable(base).breadthFirst();
+
+    let acc = true;
+    for (const i of iterable) {
+      acc = acc && i === expected.shift();
+    }
+    assert(acc && expected.length === 0);
+  });
+});
