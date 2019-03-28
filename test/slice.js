80728e814e1c502803fbd9d8d6264d741ff92a32
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 16:46:14 +0800

    Create slice.js

---
+++ test/slice.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#slice', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.slice();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is an invalid number', () => {
+    try {
+      Iterable.slice([1, 2, 3]);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is a non-positive number', () => {
+    try {
+      Iterable.slice([1, 2, 3], -1);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is a second invalid number', () => {
+    try {
+      Iterable.slice([1, 2, 3], 0);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is a second non-positive number', () => {
+    try {
+      Iterable.slice([1, 2, 3], 0, -1);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.slice([1, 2, 3, 4], 0, 4);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct value', () => {
+    const base = [1, 2, 3, 4];
+    const expected = [3, 4];
+    const iterable = new Iterable(base).slice(2, 4);
+
+    let acc = true;
+    for (const i of iterable) {
+      acc = acc && i === expected.shift();
+    }
+    assert(acc && expected.length === 0);
+  });
+});
