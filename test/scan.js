cd589ff080c4342176dc1fdf4623e3560ff6e0bc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:33:05 +0800

    Create scan.js

---
+++ test/scan.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#scan', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.scan();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is an invalid predicate', () => {
+    try {
+      Iterable.scan([1, 2, 3]);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.scan([1, 2, 3], (acc, i) => acc + i);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield the correct result.', () => {
+    const base = [1, 2, 3];
+    const expected = [1, 3, 6];
+    const iterable = new Iterable(base).scan((acc, i) => (acc == null ? 0 : acc) + i);
+    let acc = true;
+    for (const i of iterable) {
+      acc = acc && i === expected.shift();
+    }
+    assert(acc && expected.length === 0);
+  });
+});
