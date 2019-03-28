bcb0cbdba6bdaa818a26cebcf39d559c38961af6
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:22:28 +0800

    Create outerJoin.js

---
+++ src/internal/outerJoin.js
@@ -0,0 +1,32 @@
+/* eslint-disable func-names */
+/* eslint-disable no-restricted-syntax */
+import { defineField, DoubleIterableCheck } from './utils';
+import Iterable from '../iterable';
+import toArray from './toArray';
+/**
+ * @ignore
+ */
+const FIELD = defineField('outerJoin');
+/**
+ * @ignore
+ */
+export default (iterable, other) => {
+  DoubleIterableCheck(iterable, other, FIELD);
+
+  return new Iterable(function* () {
+    const A = toArray(iterable);
+    const B = toArray(other);
+
+    for (const i of A) {
+      if (!B.includes(i)) {
+        yield i;
+      }
+    }
+
+    for (const o of B) {
+      if (!A.includes(o)) {
+        yield o;
+      }
+    }
+  });
+};
