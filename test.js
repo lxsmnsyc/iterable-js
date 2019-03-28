21701b780d7a6a574d4a846332eb5b53f112ce7b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 17:22:31 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-restricted-syntax */
 const Iterable = require('./index.js');
 
-const A = [1, 2, 2, 3, 4];
-const B = [6, 4, 4, 2];
-
-for (const i of Iterable.intersect(B, A)) {
-  console.log(i);
+for (const c of Iterable.range(3, 1).scanRight((a, b) => a + b)) {
+  console.log(c);
 }
