45b1d846df3b9efdd442222f3239914cfc287ad4
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:22:35 +0800

    Update slice.js

--- src/internal/slice.js
+++ src/internal/slice.js
@@ -1,7 +1,12 @@
-import { defineField, PositiveNumberCheck } from './utils';
+import { defineField, PositiveNumberCheck, IterableCheck } from './utils';
+import skip from './skip';
+import take from './take';
 
 const FIELD = defineField('slice');
 export default (iterable, start, end) => {
+  IterableCheck(iterable, 1, FIELD);
   PositiveNumberCheck(start, 2, FIELD);
   PositiveNumberCheck(end, 3, FIELD);
+
+  return skip(take(iterable, end), start);
 };
