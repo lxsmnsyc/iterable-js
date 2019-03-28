855b61c1e9c2c04b692bad82d76dd6c25915329f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 13:22:53 +0800

    Update intersperse.js

--- src/internal/intersperse.js
+++ src/internal/intersperse.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable func-names */
 import { IterableCheck, defineField } from './utils';
-import reduce from './reduce';
+import Iterable from '../iterable';
 /**
  * @ignore
  */
@@ -9,13 +11,16 @@ const FIELD = defineField('intersperse');
  */
 export default (iterable, value) => {
   IterableCheck(iterable, 1, FIELD);
-
-  return reduce(iterable, (acc, item) => {
-    if (typeof acc === 'undefined') {
-      return [item];
+  return new Iterable(function* () {
+    const buffer = [];
+    let prev;
+    for (const i of iterable) {
+      buffer.push(i);
+      prev = buffer.slice(0);
+      buffer.push(value);
     }
-    acc.push(value);
-    acc.push(item);
-    return acc;
+    for (const i of prev) {
+      yield i;
+    }
   });
 };
