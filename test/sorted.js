ebe67d14083526e3624dda06bb2ab3d6a041663d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:33:08 +0800

    Create sorted.js

---
+++ test/sorted.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#sorted', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.sorted();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should return an Iterable if no errors.', () => {
+    const iterable = Iterable.sorted([1, 2, 3, 4]);
+    assert(iterable instanceof Iterable);
+  });
+  it('should yield true if the sequence is sorted by ascending.', () => {
+    const base = [1, 2, 3, 4];
+    const iterable = new Iterable(base).sorted();
+    for (const c of iterable) {
+      assert(c === true);
+    }
+  });
+  it('should yield true if the sequence is sorted by comparator.', () => {
+    const base = [1, 2, 3, 4];
+    const iterable = new Iterable(base).sorted((a, b) => a - b);
+    for (const c of iterable) {
+      assert(c === true);
+    }
+  });
+  it('should yield false if the sequence is not sorted by ascending.', () => {
+    const base = [1, 3, 2, 4];
+    const iterable = new Iterable(base).sorted();
+    for (const c of iterable) {
+      assert(c === false);
+    }
+  });
+  it('should yield false if the sequence is not sorted by comparator.', () => {
+    const base = [1, 2, 3, 4];
+    const iterable = new Iterable(base).sorted((a, b) => b - a);
+    for (const c of iterable) {
+      assert(c === false);
+    }
+  });
+});
