f55bb07000f9d9c1316168fbcb82925045251bac
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:22:25 +0800

    Update intersect.js

--- src/internal/intersect.js
+++ src/internal/intersect.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-restricted-syntax */
 import { defineField, DoubleIterableCheck } from './utils';
 import Iterable from '../iterable';
+import toArray from './toArray';
 /**
  * @ignore
  */
@@ -13,11 +14,12 @@ export default (iterable, other) => {
   DoubleIterableCheck(iterable, other, FIELD);
 
   return new Iterable(function* () {
-    for (const i of iterable) {
-      for (const o of other) {
-        if (i === o) {
-          yield i;
-        }
+    const A = toArray(iterable);
+    const B = toArray(other);
+
+    for (const i of A) {
+      if (B.includes(i)) {
+        yield i;
       }
     }
   });
