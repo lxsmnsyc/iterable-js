e7ae857ee09a59492782bf4423edfc7aae065ea1
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:22:27 +0800

    Create leftJoin.js

---
+++ src/internal/leftJoin.js
@@ -0,0 +1,30 @@
+/* eslint-disable func-names */
+/* eslint-disable no-restricted-syntax */
+import { defineField, DoubleIterableCheck } from './utils';
+import Iterable from '../iterable';
+import toArray from './toArray';
+/**
+ * @ignore
+ */
+const FIELD = defineField('leftJoin');
+/**
+ * @ignore
+ */
+export default (iterable, other) => {
+  DoubleIterableCheck(iterable, other, FIELD);
+
+  return new Iterable(function* () {
+    const A = toArray(iterable);
+    const B = toArray(other);
+
+    for (const i of A) {
+      yield i;
+    }
+
+    for (const o of B) {
+      if (A.includes(o)) {
+        yield o;
+      }
+    }
+  });
+};
