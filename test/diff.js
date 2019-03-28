1bc2a9150737b0135bc1344642bb031095c2dd03
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:32:27 +0800

    Create diff.js

---
+++ test/diff.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-restricted-syntax */
+import '@babel/polyfill';
+import assert from 'assert';
+import Iterable from '../src/iterable';
+import { BadArgumentError } from '../src/internal/utils';
+
+/* eslint-disable no-undef */
+describe('#diff', () => {
+  it('should throw a BadArgumentError if there is an invalid Iterable', () => {
+    try {
+      Iterable.diff();
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should throw a BadArgumentError if there is a second invalid Iterable', () => {
+    try {
+      Iterable.diff(Iterable.just(10));
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
+  it('should yield the correct sequence', () => {
+    const expected = [1, 3];
+    const iterable = new Iterable([1, 2, 2, 3, 4]).diff([6, 4, 4, 2]);
+    let acc = true;
+    for (const i of iterable) {
+      acc = acc && i === expected.shift();
+    }
+    assert(acc && expected.length === 0);
+  });
+});
