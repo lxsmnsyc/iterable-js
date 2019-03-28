ef6bc00627aa9801d0d2c3fcb30b29ddbe872e7a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 15:22:20 +0800

    Create diff.js

---
+++ src/internal/diff.js
@@ -0,0 +1,26 @@
+/* eslint-disable func-names */
+/* eslint-disable no-restricted-syntax */
+import { defineField, DoubleIterableCheck } from './utils';
+import Iterable from '../iterable';
+import toArray from './toArray';
+/**
+ * @ignore
+ */
+const FIELD = defineField('diff');
+/**
+ * @ignore
+ */
+export default (iterable, other) => {
+  DoubleIterableCheck(iterable, other, FIELD);
+
+  return new Iterable(function* () {
+    const A = toArray(iterable);
+    const B = toArray(other);
+
+    for (const i of A) {
+      if (!B.includes(i)) {
+        yield i;
+      }
+    }
+  });
+};
