94ac03d82cc0b59f756ea6711519e951a7a6fb54
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 16:46:10 +0800

    Update sorted.js

--- test/sorted.js
+++ test/sorted.js
@@ -13,6 +13,13 @@ describe('#sorted', () => {
       assert(e instanceof BadArgumentError);
     }
   });
+  it('should throw a BadArgumentError if there is a non-function provided (except undefined)', () => {
+    try {
+      Iterable.sorted([1, 2, 3], 1);
+    } catch (e) {
+      assert(e instanceof BadArgumentError);
+    }
+  });
   it('should return an Iterable if no errors.', () => {
     const iterable = Iterable.sorted([1, 2, 3, 4]);
     assert(iterable instanceof Iterable);
