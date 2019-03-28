1c5b7ee8490514be5f5f801702f959c7adbaa243
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-28 18:11:08 +0800

    Create whileDo.js

---
+++ src/internal/whileDo.js
@@ -0,0 +1,23 @@
+/* eslint-disable func-names */
+/* eslint-disable no-restricted-syntax */
+
+import Iterable from '../iterable';
+import { defineField, IterablePredicateCheck } from './utils';
+
+/**
+ * @ignore
+ */
+const FIELD = defineField('filter');
+/**
+ * @ignore
+ */
+export default (iterable, predicate) => {
+  IterablePredicateCheck(iterable, predicate, FIELD);
+  return new Iterable(function* () {
+    while (predicate()) {
+      for (const i of iterable) {
+        yield i;
+      }
+    }
+  });
+};
